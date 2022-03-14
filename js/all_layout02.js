21bdb2e2c3731d932849704704f19e532ab218e4
박호상 <100889746+hosangpark@users.noreply.github.com>
2022-03-14 16:03:46 +0900

    Update all_layout02.js

--- js/all_layout02.js
+++ js/all_layout02.js
@@ -6,5 +6,13 @@ $(function(){
     autoplayspeed:1000,
     dots:true,
 });
-});
 
+
+    $('.mainVisual .slideArrows i:first-child').on('click',function(){
+        $('.mainSlide').slick('slickPrev')
+    });
+    $('.mainVisual .slideArrows i:last-child').on('click',function(){
+        $('.mainSlide').slick('slickNext')
+    });
+
+});
