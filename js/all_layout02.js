6ec55f66363e5bf87ad11fffbb30b606334b955b
박호상 <100889746+hosangpark@users.noreply.github.com>
2022-03-14 15:45:10 +0900

    Add files via upload

--- js/all_layout02.js
+++ js/all_layout02.js
@@ -1,3 +1,10 @@
-document.querySelector('main').style.display = 'none';
+$(function(){
 
-/* window.addEventListener('DomcontentLoaded', ()=>{})  바닐라js*/
+    $('.mainSlide').slick({
+    arrows:false, /* 슬라이드 좌우 버튼 */
+    autoplay:true,
+    autoplayspeed:1000,
+    dots:true,
+});
+});
+
