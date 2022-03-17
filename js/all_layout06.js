9eeedd4f29b71c5924a575c358d80e81908dd4dc
박호상 <100889746+hosangpark@users.noreply.github.com>
2022-03-17 15:55:43 +0900

    Add files via upload

--- js/all_layout06.js
+++ js/all_layout06.js
@@ -6,4 +6,32 @@ $(function(){
     $('.topBanner').slideUp();}
     $('.topBanner .container i').on('click', topBannerHandler)
     
+
+
+$('.visualSlide').slick({
+    arrows:false,
+    autoplay:true,
+    autoplayspeed:2000,
+    dots:true,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
