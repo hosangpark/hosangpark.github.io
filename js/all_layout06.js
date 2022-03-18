63710598e0790195ebe9fa2dd5dccd3e4db94f35
박호상 <100889746+hosangpark@users.noreply.github.com>
2022-03-18 18:24:25 +0900

    Add files via upload

--- js/all_layout06.js
+++ js/all_layout06.js
@@ -1,37 +1,70 @@
 $(function(){
+  // topBanner:22.03.17 //  
 
-    // topBanner:22.03.17 //
-    function topBannerHandler(){
-      //  $('.topBanner').hide();  display:none;//
-    $('.topBanner').slideUp();}
-    $('.topBanner .container i').on('click', topBannerHandler)
-    
-
-
+  function topBannerHandler() {
+      // $('.topBannner').hide(); //display:none;
+      $('.topBanner').slideUp();}
+  $('.topBanner .container i').on('click', topBannerHandler);
+  
+  
 $('.visualSlide').slick({
-    arrows:false,
-    autoplay:true,
-    autoplayspeed:2000,
-    dots:true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    prevArrow: '    <div class="msLeft"></div>      ',
+    nextArrow: '    <div class="msRight"></div>      '
 });
 
 
+// 슬라이드에 애니메이션...
+$('.visualSlide figure').eq(1).addClass('oo');
+$('.visualSlide').on('afterChange', function (e,s,c){
+    $('.visualSlide figure').eq(c+1).addClass('oo').siblings().removeClass('oo');
+    if (c===1) {
+        $('.msLeft').addClass('oo')
+    } else {
+        $('.msLeft').removeClass('oo')
+    }
+});
+
+//
+
+$('.eProductSlide').slick({
+    arrows: false,
+    autoplay: true, 
+    autoplaySpeed: 1000,
+});
 
 
+$('.evenetProduct i:nth-of-type(1)').on('click', function(){
+    $('.eProductSlide').slick('slickPause')
+});
+
+$('.evenetProduct i:nth-of-type(2)').on('click', function(){
+    $('.eProductSlide').slick('slickPlay')
+});
+
+//
+
+$('.aproductSlide').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 5,
+    centerMode: true,
+    autoplay:true,
+    autoplaySpeed: 1000,
+});
+
+$('.allProduct i:first-child').on('click', function(){
+    $('.aproductSlide').slick('slickNext')
+});
+
+$('.allProduct i:last-child').on('click', function(){
+    $('.aproductSlide').slick('slickPrev')
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+  
+  //////////////////////////////////
+  })
