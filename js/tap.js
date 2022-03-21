529c86cf4f7f12e0281ee1b3fb899d9b107be96f
박호상 <100889746+hosangpark@users.noreply.github.com>
2022-03-21 15:00:10 +0900

    Add files via upload

--- js/tap.js
+++ js/tap.js
@@ -1,19 +1,15 @@
 
-function moveon(){
-    var 번호 = $(this).index();    
-    $('.tabLink li').removeClass('active');
-    $(this).addClass('active');
-    $('.tabContent>div').removeClass('active');
-    $('.tabContent>div').eq(번호).addClass('active');
-//  $('.tabContent>div').eq(번호).addClass('active').sibling().removeClass('active');
-}
-
-
-
-$('.tabLink li').on('click', moveon);
-
 $('.xi-close').on('click', function(){
     $('.headerWrap').slideToggle();
 })
+
+
+
+$('.bgfixed').hover(function(){$('.footerWrap').hide()}
+,function(){$('.footerWrap').show();})
+
+//$('.bgfixed').on('mouseenter', function(){$('.footerWrap').hide();});
+//$('.bgfixed').on('mouseenter', function(){$('.footerWrap').show();});
+
 
 // console.log($(this), $(this).index()) 
